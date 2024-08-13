8bc8ca82e41301add5f527d20fb993c1280f1af2
Дмитрий Насенков <dmitry_nk@outlook.com>
2024-08-13 21:01:22 +0300

    init

--- Menu_Server/Controllers/DishesController.cs
+++ Menu_Server/Controllers/DishesController.cs
@@ -20,12 +20,30 @@ namespace Menu_Server.Controllers
         }
 
         [HttpGet]
+        [Route("get")]
         public async Task<List<DishModel>> GetDishes()
         {
             var recipes= await _repository.GetAll();
             var dishes = _sender.GetDishModelsWihoutExtras(recipes);
             dishes.AddRange(_sender.GetDishModelsWihotExtras(recipes));
             return dishes;
+        }
+        [HttpGet]
+        [Route("getall")]
+        public async Task<List<DishModel>> GetAllDishes() 
+        {
+            var recipes = await _repository.GetAll();
+            var dishes = _sender.GetDishModelsWihoutExtras(recipes);
+            dishes.AddRange(_sender.GetDishModelsWihotExtras(recipes));
+            dishes.AddRange(_sender.GetNotAvailable(recipes));
+            return dishes;
+        }
+
+        [HttpPost]
+       
+        public IActionResult PostOrder( [FromBody]List<DishModel> dishModels)
+        {
+            return Ok(dishModels);
         }
     }
 }
